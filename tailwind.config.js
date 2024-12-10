47809d92f42b9790a6b45d9c34ae5020e6235f4b
Sayan Ash <sayanash347200@gmail.com>
2024-12-10 23:09:00 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3238f2',
+      },
+      fontFamily: {
+        'display': ['Popins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
